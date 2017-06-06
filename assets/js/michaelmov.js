ca8bad9e137b0074599d6072783353b9a0d7e6c7
Michael Movsesov <michael.movsesov@gmail.com>
2017-06-05 22:18:48 -0700

    Ajaxified the whole site!

--- assets/js/michaelmov.js
+++ assets/js/michaelmov.js
@@ -4,7 +4,31 @@
 
 $(document).ready(function(){
 
-   // Toggle sidebar navigation on mobile.
+    var siteUrl = 'http://'+(document.location.hostname||document.location.host);
+
+    //	Catch all internally-focused links and push a new state.
+    //	Note: External links will not be affected by this behavior.
+    $(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
+        e.preventDefault();
+        History.pushState({}, "", this.pathname);
+    });
+
+    History.Adapter.bind(window, 'statechange', function(){
+        var state = History.getState();
+        $.get(state.url, function(data){	// Use AJAX to get the new content.
+            document.title = data.match(/<title>(.*?)<\/title>/)[1];
+            $('.inner-wrap').parent().html($(data).find('.inner-wrap')); 	// Pull the post we want out of the .content class.
+            // If you change the class of the post container,
+            // you must change it here!!!
+            // _gaq.push(['_trackPageview', state.url]);	// This updates Google Analytics with a visit to the new page.
+            // If you don't use Google Analytics, you can safety comment or
+            // remove that line.
+        });
+    });
+
+
+
+    // Toggle sidebar navigation on mobile.
     $('#hamburger').click(function(){
 
         $('#hamburger').toggleClass('open');
