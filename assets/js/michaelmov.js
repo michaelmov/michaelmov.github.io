29177b32be19d2a13741c7f4504bbc85435185c4
Michael Movsesov <michael.movsesov@gmail.com>
2017-06-16 00:23:22 -0700

    Fixed sidebar behavior on mobile.

--- assets/js/michaelmov.js
+++ assets/js/michaelmov.js
@@ -5,68 +5,75 @@
 $(document).ready(function(){
 
     'use strict';
+
+    var hamburger = $('#hamburger'),
+        link = $('a'),
+        sidebar = $('.sidebar');
+
     bindClickEvents();
-
-    var page = $('#main'),
-        options = {
-            debug: true,
-            prefetch: true,
-            cacheLength: 4,
-            onStart: {
-                duration: 350, // Duration of our animation
-                render: function ($container) {
-                    // Add your CSS animation reversing class
-                    $container.addClass('is-exiting');
-                    // Restart your animation
-                    smoothState.restartCSSAnimations();
-                }
-            },
-            onReady: {
-                duration: 0,
-                render: function ($container, $newContent) {
-                    // Remove your CSS animation reversing class
-                    $container.removeClass('is-exiting');
-                    // Inject the new content
-                    $container.html($newContent);
-                }
-            },
-            onAfter: function ($container, $newContent) {
-                bindClickEvents();
-            }
-        },
-        smoothState = page.smoothState(options).data('smoothState');
-
-
 
     // Close sidebar navigation on vertical scroll.
     $(window).scroll(function(){
         if($(window).scrollTop() >= 100) {
-            $('#hamburger').removeClass('open');
-            $('body').removeClass('js-sidebar-open');
+            closeSidebar();
         }
     });
 
     // Close sidebar navigation when touching outside of the sidebar container on mobile devices.
-    $(document).on('touchstart', function (e)
-    {
-        var sidebar = $('.sidebar');
-
+    $(document).on('touchstart', function (e) {
         if (!sidebar.is(e.target) // if the target of the click isn't the container...
             && sidebar.has(e.target).length === 0) // ... nor a descendant of the container
         {
-            $('#hamburger').removeClass('open');
-            $('body').removeClass('js-sidebar-open');
+            closeSidebar();
         }
     });
 
     function bindClickEvents() {
-        var hamburger = $('#hamburger');
-
-        $(hamburger).click(function(){
+        $(hamburger).on('click', function() {
             hamburger.toggleClass('open');
             $('body').toggleClass('js-sidebar-open');
         });
+
+        $(link).on('click', function() {
+            closeSidebar();
+        })
+    }
+
+    function closeSidebar() {
+        $(hamburger).removeClass('open');
+        $('body').removeClass('js-sidebar-open');
     }
 
 
 });
+
+
+var page = $('#main'),
+    options = {
+        debug: true,
+        prefetch: true,
+        cacheLength: 4,
+        onStart: {
+            duration: 350, // Duration of our animation
+            render: function ($container) {
+                // Add your CSS animation reversing class
+                $container.addClass('is-exiting');
+                // Restart your animation
+                smoothState.restartCSSAnimations();
+            }
+        },
+        onReady: {
+            duration: 0,
+            render: function ($container, $newContent) {
+                // Remove your CSS animation reversing class
+                $container.removeClass('is-exiting');
+                // Inject the new content
+                $container.html($newContent);
+            }
+        },
+        onAfter: function ($container, $newContent) {
+            bindClickEvents();
+        }
+    },
+
+    smoothState = page.smoothState(options).data('smoothState');
