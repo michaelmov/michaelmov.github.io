73f0e1b4d2ba04a16e907177ff2e4af222d3cdbe
Michael Movsesov <michael.movsesov@gmail.com>
2017-06-18 11:42:35 -0700

    Refactored JS spaghetti code into a clearly defined object :).

--- assets/js/michaelmov.js
+++ assets/js/michaelmov.js
@@ -2,74 +2,84 @@
  * Created by Michael Movsesov on 2/14/16.
  */
 
-$(document).ready(function(){
+var Site = {
+    init: function () {
+        // Cache UI elements.
+        Site.hamburger = $('#hamburger');
+        Site.link = $('a');
+        Site.sidebar = $('.sidebar');
+        Site.page = $('#main');
 
-    'use strict';
+        // Close sidebar navigation on vertical scroll.
+        $(window).scroll(function(){
+            if($(window).scrollTop() >= 100) {
+                Site.closeSidebar();
+            }
+        });
 
-    var hamburger = $('#hamburger'),
-        link = $('a'),
-        sidebar = $('.sidebar');
+        this.bindClickEvents();
+        this.ajaxify();
+    },
 
-    bindClickEvents();
-
-
-
-    // Close sidebar navigation on vertical scroll.
-    $(window).scroll(function(){
-        if($(window).scrollTop() >= 100) {
-            closeSidebar();
-        }
-    });
-
-
-    function bindClickEvents() {
-
-        hamburger.on('click', function() {
-            hamburger.toggleClass('open');
+    bindClickEvents: function () {
+        // Turn hamburger icon to "X" and open sidebar on click.
+        this.hamburger.on('click', function(e) {
+            $(this).toggleClass('open');
             $('body').toggleClass('js-sidebar-open');
         });
 
-        link.on('click', function() {
-            closeSidebar();
-            sidebar.addClass('scene_element--width');
-        });
+        this.link.on('click', function() {
+            // If mobile sidebar is open, then close it on any link click.
+            Site.closeSidebar();
+            // Also, animate splash page into sidebar when a link is clicked on homepage.
+            this.sidebar.addClass('scene_element--width');
+        }.bind(this));
 
-        // Close sidebar navigation when touching outside of the sidebar container on mobile devices.
+        // Close mobile sidebar navigation when touching outside of the sidebar container on mobile devices.
         $('.page-content-wrapper').on('touchstart', function (e) {
-          closeSidebar();
+            Site.closeSidebar();
         });
-    }
+    },
 
-    function closeSidebar() {
-        hamburger.removeClass('open');
+    // Close sidebar and animate "X" back into hamburger symbol.
+    closeSidebar: function () {
         $('body').removeClass('js-sidebar-open');
+        this.hamburger.removeClass('open');
+    },
+
+    // Smooth page transitions without a refresh :)
+    ajaxify: function () {
+        var options = {
+            debug: true,
+            prefetch: true,
+            cacheLength: 4,
+            onStart: {
+                duration: 350, // Duration of our animation
+                render: function ($container) {
+                    // Add your CSS animation reversing class
+                    $container.addClass('is-exiting');
+                    // Restart your animation
+                    Site.smoothState.restartCSSAnimations();
+                }
+            },
+            onReady: {
+                duration: 0,
+                render: function ($container, $newContent) {
+                    // Remove your CSS animation reversing class
+                    $container.removeClass('is-exiting');
+                    // Inject the new content
+                    $container.html($newContent);
+                }
+            }
+        };
+
+        Site.smoothState = this.page.smoothState(options).data('smoothState');
     }
+};
+
+
+$(document).ready(function () {
+   Site.init();
 });
 
 
-var page = $('#main'),
-    options = {
-        debug: true,
-        prefetch: true,
-        cacheLength: 4,
-        onStart: {
-            duration: 350, // Duration of our animation
-            render: function ($container) {
-                // Add your CSS animation reversing class
-                $container.addClass('is-exiting');
-                // Restart your animation
-                smoothState.restartCSSAnimations();
-            }
-        },
-        onReady: {
-            duration: 0,
-            render: function ($container, $newContent) {
-                // Remove your CSS animation reversing class
-                $container.removeClass('is-exiting');
-                // Inject the new content
-                $container.html($newContent);
-            }
-        }
-    },
-
-smoothState = page.smoothState(options).data('smoothState');
