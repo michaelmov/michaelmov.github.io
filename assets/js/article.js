f569a48512b934be7e89a3eca256c34967b5b961
Michael Movsesov <michael.movsesov@gmail.com>
2018-06-07 11:40:42 -0700

    Added parallax animation to article header.

---
+++ assets/js/article.js
@@ -0,0 +1,23 @@
+
+
+// /**
+//  * Created by Michael Movsesov on 2/14/16.
+//  */
+ 
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function(e) {
+        var articleHeroText = document.querySelector('.article__hero-text ');
+        var heroHeight = document.querySelector('.article__hero').clientHeight;
+        var scroll = window.scrollY;
+      
+        if(scroll <= heroHeight) {
+          articleHeroText.style.transform = 'translate(0px, ' + (scroll/4) + '%)';
+          articleHeroText.style.opacity = 1 - scroll / 250;
+        }
+        
+      });
+});
+
+
+
+
