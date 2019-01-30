4e97708c9eed7381299cd5eaa3f9eb9aae4035f4
Michael Movsesov <michael.movsesov@gmail.com>
2019-01-30 13:25:53 -0800

    Updated description

--- webpack/article.js
+++ webpack/article.js
@@ -1,7 +1,6 @@
 import hljs from 'highlight.js';
- 
-document.addEventListener("DOMContentLoaded", function(){
 
+document.addEventListener('DOMContentLoaded', function() {
     const articleHeroSection = document.querySelector('.article__hero');
     const articleHeroText = document.querySelector('.article__hero-text ');
     const heroHeight = articleHeroSection.clientHeight;
@@ -12,20 +11,19 @@ document.addEventListener("DOMContentLoaded", function(){
     // Header parallax
     window.addEventListener('scroll', e => {
         const scroll = window.scrollY;
-      
-        if(scroll <= heroHeight) {
-          articleHeroText.style.transform = `translate(0px, ${scroll/3.5}%)`;
-          articleHeroText.style.opacity = 1 - scroll / 250;    
-        }
-        
-      });
 
-      // Make all article images 100% width
-      const articleImages = document.querySelectorAll('#article-wrapper img');
-      articleImages.forEach(image => { 
-        if(image.parentElement.nodeName === 'P') {
-          image.parentElement.style.width = '100%';
+        if (scroll <= heroHeight) {
+            articleHeroText.style.transform = `translate(0px, ${scroll /
+                3.5}%)`;
+            articleHeroText.style.opacity = 1 - scroll / 250;
         }
-      });
+    });
+
+    // Make all article images 100% width
+    const articleImages = document.querySelectorAll('#article-wrapper img');
+    articleImages.forEach(image => {
+        if (image.parentElement.nodeName === 'P') {
+            image.parentElement.style.width = '100%';
+        }
+    });
 });
- 
